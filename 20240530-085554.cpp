8c6c51618b9e2e45a552eed1ed4e1352ac51b25e
Niggersaurus <brianpaolotumala5@gmail.com>
2024-06-28 19:46:52 +0800

    Add files via upload

---
+++ 20240530-085554.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    First_function()
+    Second_function()
+    int a, b, c;
+    float d, e, f;
+    cout << "Hello Brotha what you wanna add?"   ;
+    cout << "\n Type any integer here: .\n ";
+    cin >> a;
+    cout << "\n Type another integer here\n ";
+    cin >> b ;
+    c = a+b;
+    cout << " The sum is " << c ;
+    return 0;
+}
+First_function()
+{
+    int a, b, c;
+    float d, e, f;
+    cout << "Hello Brotha what you wanna add?"   ;
+    cout << "\n Type any integer here: .\n ";
+    cin >> a;
+    cout << "\n Type another integer here\n ";
+    cin >> b ;
+    c = a+b;
+    cout << " The sum is " << c ;
+    return 0;
+    }
+ Second_function()
+ {
+ 
+    
